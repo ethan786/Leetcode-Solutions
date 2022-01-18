2138494a589f97e49cbd0eac1a28184f2c9f5f31
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-18 20:47:40 +0530

    Time: 6 ms (6.07%), Space: 5.9 MB (53.80%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -17,21 +17,36 @@ public:
     //     return c;
     // }
     
-    int solve(int n,vector<int> &v){
-        if(n==0){
-            return 1;
-        }else if(n<0){
-            return 0;
+    //memoziation
+//     int solve(int n,vector<int> &v){
+//         if(n==0){
+//             return 1;
+//         }else if(n<0){
+//             return 0;
+//         }
+//         if(v[n]!=-1) return v[n];
+//         int op1 = solve(n-1,v);
+//         int op2 = solve(n-2,v);
+//         return v[n] = op1+op2;
+//     }
+    
+//     int climbStairs(int n) {
+//         vector<int> v(n+1,-1);
+//         solve(n,v);
+//         return v[n];
+//     }
+    int climbStairs(int n) {
+         //tabulation using variable as we know that we only need previous 2 values toh generate solution
+        if(n==1||n==2){
+            return n;
         }
-        if(v[n]!=-1) return v[n];
-        int op1 = solve(n-1,v);
-        int op2 = solve(n-2,v);
-        return v[n] = op1+op2;
+        int a = 1,b=2;
+        for(int i=3;i<=n;i++){
+            int c = a+b;
+            a = b;
+            b=c;
+        }
+        return b;
     }
     
-    int climbStairs(int n) {
-        vector<int> v(n+1,-1);
-        solve(n,v);
-        return v[n];
-    }
 };
