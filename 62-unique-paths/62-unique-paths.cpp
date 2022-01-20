260ddd3d8051705e22af2cab0afb5eda5763dc7e
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-21 01:34:03 +0530

    Time: 3 ms (23.87%), Space: 9.8 MB (23.48%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -14,6 +14,18 @@ public:
     }
     int uniquePaths(int m, int n) {
         vector<vector<int>> dp(101,vector<int>(101,-1));
-        return path(n,m,0,0,dp);
+        //return path(n,m,0,0,dp);
+        for(int i=0;i<n;i++){
+            dp[i][0] = 1;
+        }
+        for(int i=0;i<m;i++){
+            dp[0][i] = 1;
+        }
+        for(int i=1;i<n;i++){
+            for(int j=1;j<m;j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n-1][m-1];
     }
 };
