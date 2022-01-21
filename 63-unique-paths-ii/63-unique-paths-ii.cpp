9fe0ee94c6d4cf40146dc19be53afebef0536be4
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-21 18:01:34 +0530

    Time: 8 ms (19.31%), Space: 10 MB (11.38%) - LeetHub

--- 63-unique-paths-ii/63-unique-paths-ii.cpp
+++ 63-unique-paths-ii/63-unique-paths-ii.cpp
@@ -1,18 +1,22 @@
 class Solution {
 public:
-    int m, n;
-vector<vector<int> > dp;
-int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-	m = size(obstacleGrid), n = size(obstacleGrid[0]);
-	dp.resize(m, vector<int>(n));
-	return solve(obstacleGrid, 0, 0);
-}
-// function to recursively explore all unique paths and store the results once calculated
-int solve(vector<vector<int>>& grid, int i, int j) {
-	if(i < 0 || j < 0 || i >= m || j >= n) return 0;    // bounds checking
-	if(grid[i][j]) return dp[i][j] = 0;                 // obstacle found at current cell
-	if(i == m - 1 && j == n - 1) return 1;              // reached bottom-right of grid ? return 1
-	if(dp[i][j]) return dp[i][j];                       // if already computed for current cell, just return the stored results
-	return dp[i][j] = solve(grid, i + 1, j) + solve(grid, i, j + 1); // recursively explore the two options available with us
-}
+    
+    int path(vector<vector<int>>& obstacleGrid,int n,int m,int i,int j,vector<vector<int>> &dp){
+        if(i==n-1 and j == m-1 and obstacleGrid[i][j] == 0){
+            return 1;
+        }else if(i>=n || j>=m || obstacleGrid[i][j] == 1 ){
+            return 0;
+        }
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        return dp[i][j] = path(obstacleGrid,n,m,i+1,j,dp) + path(obstacleGrid,n,m,i,j+1,dp);
+    }
+    
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size();
+        int m = obstacleGrid[0].size();
+        vector<vector<int>> dp(101,vector<int>(101,-1));
+        return path(obstacleGrid,n,m,0,0,dp);
+    }
 };
