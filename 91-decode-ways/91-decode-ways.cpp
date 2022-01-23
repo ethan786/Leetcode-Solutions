20bab5f0181e918ad7659fee9c8b9a8e5e5a90ff
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-23 14:50:28 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (42.36%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,27 +1,20 @@
 class Solution {
 public:
-    
-     int m = 1e9+7;
-	    int solve(int i,string &s,vector<int> &dp){
-	        if(i==s.size()){
-	            return 1;
-	        }
-	        long long op1 = 0;
-	        
-	        if(dp[i] !=-1) return dp[i];
-	       
-	        if(s[i] != '0'){
-	         op1 = solve(i+1,s,dp);
-	        }
-	        if(i < s.size()-1 and (s[i]-'0')*10 + (s[i+1]-'0') <=26 and (s[i]-'0') > 0){
-	        op1 += solve(i+2,s,dp);
-	        }
-	        return dp[i] = op1;
-	    }
-    
     int numDecodings(string s) {
+        int n = s.size();
+        vector<int> dp(n,0);
         if(s[0] == '0') return 0;
-		    vector<int> dp(s.size()+1,-1);
-		    return solve(0,s,dp);
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            int one_digit = (s[i]-'0');
+            int two_digit = (s[i-1]-'0')*10 + (s[i]-'0');
+            //cout<<one_digit<<" "<<two_digit;
+            if(one_digit > 0) dp[i] = dp[i-1];
+            if(i>=2 and two_digit < 27 and two_digit >=10) dp[i] += dp[i-2];
+            if(i==1 and two_digit < 27 and two_digit >=10){
+                 dp[i]+=1;
+            }
+        }
+        return dp[n-1];
     }
 };
