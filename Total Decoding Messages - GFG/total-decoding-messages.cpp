9490fc761c563b04af560ac1c464dd56d24ce54b
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-23 14:51:23 +0530

    Added solution - LeetHub

--- Total Decoding Messages - GFG/total-decoding-messages.cpp
+++ Total Decoding Messages - GFG/total-decoding-messages.cpp
@@ -6,27 +6,24 @@ using namespace std;
 class Solution {
 	public:
 	    int m = 1e9+7;
-	    int solve(int i,string &s,vector<int> &dp){
-	        if(i==s.size()){
-	            return 1;
-	        }
-	        long long op1 = 0;
-	        
-	        if(dp[i] !=-1) return dp[i]%m;
-	       
-	        if(s[i] != '0'){
-	         op1 = solve(i+1,s,dp)%m;
-	        }
-	        if(i < s.size()-1 and (s[i]-'0')*10 + (s[i+1]-'0') <=26 and (s[i]-'0') > 0){
-	        op1 += solve(i+2,s,dp)%m;
-	        }
-	        return dp[i] = op1%m;
-	    }
+	    
 	     
-		int CountWays(string str){
-		    if(str[0] == '0') return 0;
-		    vector<int> dp(str.size(),-1);
-		    return solve(0,str,dp);
+		int CountWays(string s){
+		    int n = s.size();
+        vector<int> dp(n,0);
+        if(s[0] == '0') return 0;
+        dp[0]=1;
+        for(int i=1;i<n;i++){
+            int one_digit = (s[i]-'0');
+            int two_digit = (s[i-1]-'0')*10 + (s[i]-'0');
+            //cout<<one_digit<<" "<<two_digit;
+            if(one_digit > 0) dp[i] = dp[i-1]%m;
+            if(i>=2 and two_digit < 27 and two_digit >=10) dp[i] += dp[i-2]%m;
+            if(i==1 and two_digit < 27 and two_digit >=10){
+                 dp[i]+=1;
+            }
+        }
+        return dp[n-1]%m;
 		}
 
 };
