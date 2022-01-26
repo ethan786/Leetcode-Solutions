3c1f9f7f6c984e3f1918fe621e18cae93da51680
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-01-26 21:38:11 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (89.57%) - LeetHub

--- 343-integer-break/343-integer-break.cpp
+++ 343-integer-break/343-integer-break.cpp
@@ -1,26 +1,15 @@
 class Solution {
 public:
     int integerBreak(int n) {
-        int ans =INT_MIN;
-        if(n==2) return 1;
-       for(int i=2;i<n;i++){
-           int x = n/i;
-           int t = x;
-           int rem = n%i;
-           int product = 1;
-           int in = i;
-           while(rem>0){
-               product = product * (x+1);
-               rem--;
-               in--;
-           }
-           while(in){
-               product = product * t;
-               in--;
-           }
-           //cout<<i<<" "<<product<<endl;
-           ans = max(ans,product);
-       }
-        return ans;
+        
+        if(n<=3) return n-1;
+        
+        int pro = 1;
+        while(n>4){
+            n-=3;
+            pro *= 3;
+        }
+        
+        return pro*n;
     }
 };
