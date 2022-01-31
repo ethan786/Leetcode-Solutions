dbf864ffcdeed9fa3a711ec78d891013eaaa7048
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-01 00:41:03 +0530

    Time: 477 ms (16.77%), Space: 10.7 MB (12.95%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -2,65 +2,22 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
-        // vector<int> dp(n+1,0);
-        // dp[0] = 1;
-        // if(n == 1){
-        //     return dp[0];
-        // }
-        // for(int i=1;i<n;i++){
-        //     dp[i] = 1;
-        //     for(int j=i-1;j>=0;j--){
-        //         if(nums[j] >= nums[i]) continue;
-        //         int possibleAns = dp[j] + 1;
-        //         if(possibleAns > dp[i]){
-        //             dp[i] = possibleAns;
-        //         }
-        //     }
-        // }
-        // int ans = -1;
-        // for(int i=0;i<n;i++){
-        //     ans = max(dp[i],ans);
-        // }
-        vector<int> v;
-        v.push_back(nums[0]);
-        for(int i=1;i<n;i++){
-            int ele = nums[i];
-            auto lb = lower_bound(v.begin(),v.end(),ele);
-            if(lb == v.end()){
-                v.push_back(ele);
-            }else{
-                int index = lb - v.begin();
-                v[index] = nums[i];
+        vector<int> dp(n,1);
+        dp[0] = 1;
+        if(n==1) return 1;
+        dp[1] = nums[0] < nums[1] ? 2 : 1;//ternary operator
+        if(n==2) return max(dp[1],dp[0]);
+        for(int i=2;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[j] + 1,dp[i]);
+                }
             }
         }
-        
-        return v.size();
+        int mx = -1e5;
+        for(int i=0;i<n;i++){
+            mx = max(dp[i],mx);
+        }
+        return mx;
     }
 };
-// 2 3 7 18
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
