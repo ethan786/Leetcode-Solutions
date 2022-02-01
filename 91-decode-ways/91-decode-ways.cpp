91a9ed64d2e976c1d4ddab8ca8517986c517dff3
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-01 21:26:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (43.23%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -2,17 +2,21 @@ class Solution {
 public:
     int numDecodings(string s) {
         int n = s.size();
+        //base case
+        if(n==1 and (s[0]-'0') > 0){
+            return 1;
+        }else if(n==1 and (s[0]-'0') == 0) return 0;
         vector<int> dp(n,0);
-        if(s[0] == '0') return 0;
-        dp[0]=1;
+        dp[0] = (s[0]-'0' > 0 ? 1 : 0);//hard coding 
         for(int i=1;i<n;i++){
-            int one_digit = (s[i]-'0');
-            int two_digit = (s[i-1]-'0')*10 + (s[i]-'0');
-            //cout<<one_digit<<" "<<two_digit;
-            if(one_digit > 0) dp[i] = dp[i-1];
-            if(i>=2 and two_digit < 27 and two_digit >=10) dp[i] += dp[i-2];
-            if(i==1 and two_digit < 27 and two_digit >=10){
-                 dp[i]+=1;
+            int single_digit = s[i]-'0';
+            int two_digit = (s[i-1]-'0')*10 + single_digit;
+            if(single_digit > 0 ) dp[i] = dp[i-1]; //base condition
+            if(i>=2 and two_digit >= 10 and two_digit <= 26){
+                dp[i] += dp[i-2];
+            }
+            if(i==1 and two_digit >= 10 and two_digit <= 26){
+                dp[i] += 1;
             }
         }
         return dp[n-1];
