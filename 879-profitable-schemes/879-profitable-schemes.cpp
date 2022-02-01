e63910ba751058e12a593814bbe6e1311e67cf37
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-01 19:18:00 +0530

    Time: 217 ms (44.90%), Space: 12.8 MB (45.58%) - LeetHub

--- 879-profitable-schemes/879-profitable-schemes.cpp
+++ 879-profitable-schemes/879-profitable-schemes.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
-    int dp[102][102][102];
-    int m = 1000000007;
-    int solve(int i,int size,int n, int mp, vector<int>& g, vector<int>& p,int x,int y){
-        if(i == size){
-            if(n >= x and mp <= y){
+    int dp[101][101][101];
+    int solve(int i,int n, int mp, vector<int>& g, vector<int>& p){
+        if(n<0){
+            return 0;
+        }
+        if(i == p.size()){
+            if(mp == 0){
                 return 1;
             }
             return 0;
         }
-        else if(x > n){
-            return 0;
-        }
         
-        if(dp[i][x][y]!=-1) return dp[i][x][y];
+        if(dp[i][n][mp] != -1) return dp[i][n][mp];
         
-        int op1 = solve(i+1,size,n,mp,g,p,x+g[i],min(y+p[i],mp));
-        int op2 = solve(i+1,size,n,mp,g,p,x,y);
-        int ans = (op1+op2)%m;
-        dp[i][x][y] = ans%m;
-        return ans%m;
+        int op1 = solve(i+1,n-g[i],max(0,mp-p[i]),g,p);
+        int op2 = solve(i+1,n,mp,g,p);
+        int ans = (op1+op2)%1000000007;
+        dp[i][n][mp] = ans;
+        return ans ;
     }
     
     int profitableSchemes(int n, int mp, vector<int>& g, vector<int>& p) {
         memset(dp,-1,sizeof(dp));
-        return solve(0,g.size(),n,mp,g,p,0,0);
+        return solve(0,n,mp,g,p);
     }
 };
