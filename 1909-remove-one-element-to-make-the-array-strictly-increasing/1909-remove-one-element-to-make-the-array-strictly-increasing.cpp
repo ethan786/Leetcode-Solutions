282affd02be95432ffb5d5cf64985ded309de178
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-05 15:31:14 +0530

    Time: 84 ms (10.60%), Space: 10.2 MB (14.76%) - LeetHub

---
+++ 1909-remove-one-element-to-make-the-array-strictly-increasing/1909-remove-one-element-to-make-the-array-strictly-increasing.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    
+    int LIS(vector<int> &nums,int n){
+        //int n = nums.size();
+        vector<int> dp(n,1);
+        dp[0] = 1;
+        if(n==1) return 1;
+        dp[1] = nums[0] < nums[1] ? 2 : 1;//ternary operator
+        if(n==2) return max(dp[1],dp[0]);
+        for(int i=2;i<n;i++){
+            for(int j=i-1;j>=0;j--){
+                if(nums[i] > nums[j]){
+                    dp[i] = max(dp[j] + 1,dp[i]);
+                }
+            }
+        }
+        int mx = -1e5;
+        for(int i=0;i<n;i++){
+            mx = max(dp[i],mx);
+        }
+        return mx;
+    }
+    
+    bool canBeIncreasing(vector<int>& nums) {
+        int n = nums.size();
+        int lis = LIS(nums,n);
+        int s = n-lis;
+        return s <= 1;
+    }
+};
