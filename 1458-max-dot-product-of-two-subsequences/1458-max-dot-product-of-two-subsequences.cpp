6bf02def833c3be43a285c848157688c9ea160d7
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-08 14:05:55 +0530

    Time: 54 ms (39.90%), Space: 14.3 MB (10.88%) - LeetHub

--- 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
+++ 1458-max-dot-product-of-two-subsequences/1458-max-dot-product-of-two-subsequences.cpp
@@ -15,7 +15,17 @@ public:
     }
     
     int maxDotProduct(vector<int>& nums1, vector<int>& nums2) {
-        memset(dp,-1,sizeof(dp));
-        return solve(nums1,nums2,nums1.size(),nums2.size());
+        // memset(dp,-1,sizeof(dp));
+        // return solve(nums1,nums2,nums1.size(),nums2.size());
+        int n = nums1.size();
+        int m = nums2.size();
+        vector<vector<int>> dp(n+1,vector<int>(m+1,INT_MIN));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                dp[i][j] = nums1[i-1]*nums2[j-1] + max(0,dp[i-1][j-1]);
+                dp[i][j] = max({dp[i][j],dp[i-1][j],dp[i][j-1]});
+            }
+        }
+        return dp[n][m];
     }
 };
