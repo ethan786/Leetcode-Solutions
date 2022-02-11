2a18460c9a1caaa2c8036a8242298b95d2de8cc4
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-11 15:41:25 +0530

    Time: 8 ms (77.01%), Space: 7.2 MB (71.57%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,44 +1,39 @@
-// class Solution {
-// public:
-//     bool checkInclusion(string s1, string s2) {
-//         int n = s1.size();
-//         int m = s2.size();
-//         int s1map[26],s2map[26];
-//         for(int i=0;i<n;i++){
-//             s1map[s1[i]-'a']++;
-//             s2map[s2[i]-'a']++;
-//         }
-//         int count = 0;
-//         for(int i=0;i<26;i++){
-//             if(s1map[i] == s2map[i]) count++;
-//         }
-//         for(int i=0;i<m-n;i++){
-//             int r = s2[i+n] - 'a',l = s2[i] - 'a';
-//             if(count == 26) return true;
-//             s2map[r]++;
-//             if(s1map[r] == s2map[r]) count++;
-//             else if(s1map[r] + 1 == s2map[r]) count--;
-//             s2map[l]--;
-//             if(s1map[l] == s2map[l]) count++;
-//             else if(s1map[l] - 1 == s2map[l]) count--;
-            
-//         }
-//         return count == 26;
-//     }
-// };
 class Solution {
 public:
+    int s1map[26],s2map[26];
     bool checkInclusion(string s1, string s2) {
-        int map[26] = {0};
-        for(char c : s1) map[c - 'a']++;
-        int j = 0, i = 0, count_chars = s1.size();
-        while(j < s2.size()){
-            if(map[s2.at(j++) - 'a']-- > 0)
-                count_chars--;
-            if(count_chars == 0) return true;
-            if(j - i == s1.size() && map[s2.at(i++) - 'a']++ >= 0)
-                count_chars++;
+        int n = s1.size();
+        int m = s2.size();
+        if(n > m) return false;
+        for(int i=0;i<n;i++){
+            s1map[(s1[i]-'a')]++;
+            s2map[(s2[i]-'a')]++;
         }
-        return false;
+        int count = 0;
+        for(int i=0;i<26;i++){
+            if(s1map[i] == s2map[i]) 
+                count++;
+        }
+        for(int i=0;i<m-n;i++){
+            int r = (s2[i+n] - 'a'),l = (s2[i] - 'a');
+            if(count == 26) 
+                return true;
+            
+            s2map[r]++;
+            
+            if(s1map[r] == s2map[r]) 
+                count++;
+            else if(s1map[r] + 1 == s2map[r]) 
+                count--;
+            
+            s2map[l]--;
+            
+            if(s1map[l] == s2map[l]) 
+                count++;
+            else if(s1map[l] - 1 == s2map[l]) 
+                count--;
+            
+        }
+        return count == 26;
     }
 };
