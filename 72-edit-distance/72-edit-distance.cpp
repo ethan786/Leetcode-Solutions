bb9bba5a1646d9f713b48e66a0edaf550979a717
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-13 16:07:08 +0530

    Time: 22 ms (42.05%), Space: 7.3 MB (82.90%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -42,10 +42,10 @@ public:
                 if(i == 0) dp[i][j] = j;
                 else if(j == 0) dp[i][j] = i;
                 else{
-                    if (s1[i - 1] == s2[j - 1]) {
-                        dp[i][j] = dp[i - 1][j - 1];
+                    if (s1[i-1] == s2[j-1]) {
+                        dp[i][j] = dp[i-1][j-1];
                     } else {
-                        dp[i][j] = min(dp[i - 1][j - 1], min(dp[i][j - 1], dp[i - 1][j])) + 1;
+                        dp[i][j] = min({dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j])}) + 1;
                     }
                 }
             }
