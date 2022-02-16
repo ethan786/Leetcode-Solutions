2d705f5a6686bc58ed89d5429c3f8fbf0c409057
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-16 22:13:01 +0530

    Time: 512 ms (14.27%), Space: 78.7 MB (7.69%) - LeetHub

---
+++ 18-4sum/18-4sum.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    set<vector<int>> uniqueCombinations;
+    
+    vector<vector<int>> fourSum(vector<int>& nums, int target) {
+        
+        uniqueCombinations.clear();
+        vector<vector<int>> ans;
+        int n = nums.size(), i, j, k, l, searchValue;
+        if( n<=3 ) return {};
+        
+        sort(nums.begin(), nums.end());
+        
+        for(int i=0;i<n;i++){
+            
+            for(int j=i+1;j<n;j++){
+                searchValue = target - (nums[i] + nums[j]);
+                k = j+1, l = n-1;
+                findTwoSum(searchValue, i, j, k, l, nums);
+            }
+        }
+        
+        for(auto &combination : uniqueCombinations){
+            ans.push_back(combination);
+        }
+        return ans;
+    }
+    
+    
+    void findTwoSum(int searchValue, int &i, int &j, int &k, int &l, vector<int> &nums){
+        
+        int sum;
+        while(k<l){
+            
+            sum = (nums[k]+nums[l]);
+            if( searchValue == sum )
+                uniqueCombinations.insert({nums[i], nums[j], nums[k], nums[l]}), k++;
+            else if( sum < searchValue ) k++;
+            else l--;
+        }
+    }
+};
