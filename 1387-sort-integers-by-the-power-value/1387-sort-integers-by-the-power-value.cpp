6f72a418e2ce84328a64396d71e8b22f5371e22c
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-19 18:28:38 +0530

    Time: 57 ms (57.70%), Space: 8.9 MB (64.60%) - LeetHub

---
+++ 1387-sort-integers-by-the-power-value/1387-sort-integers-by-the-power-value.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+   // int dp[10000000];
+    int ways(int n){
+        if(n == 1){
+            return 0;
+        }
+        //if(dp[n] != -1) return dp[n];
+        if(n%2 == 0){
+            return  1 + ways(n/2);
+        }else{
+            return  1 + ways(3*n + 1);
+        }
+    }
+    
+    int getKth(int lo, int hi, int k) {
+        //memset(dp,-1,sizeof(dp));
+        vector<pair<int,int>> v;
+      
+        for(int i=lo;i<hi+1;i++){
+            v.push_back({ways(i),i});
+        }
+        
+        sort(v.begin(),v.end());
+        
+        return v[k-1].second;
+    }
+};
