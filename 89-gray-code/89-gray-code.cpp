bf72a20a2a143d8c2a1886f70b1e524e1e2b65da
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-02-27 23:37:51 +0530

    Time: 3 ms (98.38%), Space: 11.3 MB (89.14%) - LeetHub

--- 89-gray-code/89-gray-code.cpp
+++ 89-gray-code/89-gray-code.cpp
@@ -1,19 +1,9 @@
 class Solution {
-    void utils(bitset<32>& bits, vector<int>& result, int k){
-        if (k==0) {
-            result.push_back(bits.to_ulong());
-        }
-        else {
-            utils(bits, result, k-1);
-            bits.flip(k-1);
-            utils(bits, result, k-1);
-        }
-    }
 public:
     vector<int> grayCode(int n) {
-        bitset<32> bits;
-        vector<int> result;
-        utils(bits, result, n);
-        return result;
+        vector<int> ans(1<<n);
+        for (int i=0; i<(1<<n); i++) 
+            ans[i] = i^(i>>1);
+        return ans;
     }
 };
