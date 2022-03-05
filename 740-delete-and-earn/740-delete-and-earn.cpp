636e1f55524d59f6603c1d752d0ae2c255549313
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-03-05 23:41:49 +0530

    Time: 11 ms (58.71%), Space: 11.9 MB (28.13%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int dp[10001];
-    int houseRobber(vector<int>& arr, int idx){
-        if(idx>=arr.size()) return 0;
-        if(dp[idx] != -1) return dp[idx];
-        return dp[idx] = max(arr[idx]+houseRobber(arr,idx+2),houseRobber(arr,idx+1));
+    int houseRobber(vector<int>& arr, int i){
+        if(i>=arr.size()) return 0;
+        
+        if(dp[i] != -1) return dp[i];
+        // cout<<"ans"<<idx<<arr[idx]<<endl;
+        int op1 = arr[i]+houseRobber(arr,i+2);
+        int op2 = houseRobber(arr,i+1);
+        return dp[i] = max(op1,op2);
     }
     int deleteAndEarn(vector<int>& nums) {
         memset(dp,-1,sizeof dp);
+        
         vector<int> a(10001,0);
-        for(auto num : nums) a[num] += num;
+        
+        for(auto num : nums){
+            a[num] += num;
+        }
+        
         return houseRobber(a,0);
     }
 };
