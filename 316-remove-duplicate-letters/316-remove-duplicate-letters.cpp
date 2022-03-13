ddde42da94da727e988a68bf8a18b18420d745ca
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-03-13 17:32:21 +0530

    Time: 3 ms (73.90%), Space: 6.9 MB (36.19%) - LeetHub

---
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    
+    string removeDuplicateLetters(string s) {
+        int n = s.size();
+        vector<bool> v(27,false);
+        vector<int> lastOcc(27,-1);
+        for(int i=0;i<n;i++){
+            lastOcc[s[i]-'a'] = i;
+        }
+        stack<char> st;
+        for(int i=0;i<n;i++){
+            //cout<<v[s[i]-'a']<<" ";
+            if(v[s[i]-'a'] == false){
+                while(st.size() > 0 && st.top() > s[i] && i < lastOcc[st.top()-'a']){
+                    cout<<"suar";
+                    v[st.top()-'a'] = false;
+                    st.pop();
+                }
+                st.push(s[i]);
+                v[s[i]-'a'] = true;
+            }
+        }
+        string ans = "";
+        while(!st.empty()){
+            ans += st.top();
+            st.pop();
+        }
+        reverse(ans.begin(),ans.end());
+        return ans;
+    }
+};
