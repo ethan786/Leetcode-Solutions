60bba448a04dae6b641342ffee38fcc14fd029c0
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-03-15 03:19:05 +0530

    Time: 10 ms (42.64%), Space: 10.6 MB (25.76%) - LeetHub

---
+++ 71-simplify-path/71-simplify-path.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    string simplifyPath(string path) {
+        
+        stack<string> st;
+        string res;
+        
+        for(int i = 0;  i<path.size(); ++i)
+        {
+            if(path[i] == '/')    
+                continue;
+            string temp;
+			// iterate till we doesn't traverse the whole string and doesn't encounter the last /
+            while(i < path.size() && path[i] != '/')
+            {
+				// add path to temp string
+                temp += path[i];
+                ++i;
+            }
+            if(temp == ".")
+                continue;
+			// pop the top element from stack if exists
+            else if(temp == "..")
+            {
+                if(!st.empty())
+                    st.pop();
+            }
+            else
+			// push the directory file name to stack
+                st.push(temp);
+        }
+        
+        while(!st.empty())
+        {
+            res = "/" + st.top() + res;
+            st.pop();
+        }
+        if(res.size() == 0)
+            return "/";
+        
+        return res;
+    }
+};
