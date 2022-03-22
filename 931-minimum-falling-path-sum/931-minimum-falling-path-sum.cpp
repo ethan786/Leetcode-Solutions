a5d3d2ab361dd782279751c99bb5ca22718a5141
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-03-23 02:11:54 +0530

    Time: 16 ms (72.41%), Space: 10.4 MB (22.37%) - LeetHub

--- 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
+++ 931-minimum-falling-path-sum/931-minimum-falling-path-sum.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int minFallingPathSum(vector<vector<int>>& v) {
-        int n = v.size();
-        int m = v[0].size();
+    int minFallingPathSum(vector<vector<int>>& g) {
+        int n = g.size();
+        int m = g[0].size();
         vector<vector<int>> dp(n,vector<int>(m,0));
-        //base case
         for(int i=0;i<m;i++){
-            dp[0][i] = v[0][i];
+            dp[0][i] = g[0][i];
         }
         
         for(int i=1;i<n;i++){
             for(int j=0;j<m;j++){
-                int leftup = INT_MAX;
-                if(j>0){
-                 leftup = dp[i-1][j-1];
+                int op2 = INT_MAX, op3 = INT_MAX;
+                int op1 = dp[i-1][j];
+                if(j+1 < m){
+                    op2 = dp[i-1][j+1];
                 }
-                int up = dp[i-1][j];
-                int rightup = INT_MAX;
-                if(j+1<m){
-                 rightup = dp[i-1][j+1];
+                if(j-1 >= 0){
+                    op3 = dp[i-1][j-1];
                 }
-                dp[i][j] = v[i][j] + min({leftup,up,rightup});
+                dp[i][j] = g[i][j] + min({op1,op2,op3});
             }
         }
+        
         int ans = INT_MAX;
+        
         for(int i=0;i<m;i++){
             ans = min(ans,dp[n-1][i]);
         }
+        
         return ans;
     }
 };
