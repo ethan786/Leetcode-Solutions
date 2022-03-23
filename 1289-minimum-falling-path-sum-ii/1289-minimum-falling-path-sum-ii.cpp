db30ae37b395271a6a78012ede7578b47fcb5eef
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-03-23 18:13:29 +0530

    Time: 224 ms (63.51%), Space: 15.9 MB (13.90%) - LeetHub

---
+++ 1289-minimum-falling-path-sum-ii/1289-minimum-falling-path-sum-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minFallingPathSum(vector<vector<int>>& g) {
+        int n = g.size();
+        int m = g[0].size();
+        vector<vector<int>> dp(n,vector<int>(m,0));
+        
+        for(int i=0;i<m;i++){
+            dp[0][i] = g[0][i];
+        }
+        
+        for(int i=1;i<n;i++){
+            for(int j=0;j<m;j++){
+                int mx = INT_MAX;
+                for(int k=0;k<m;k++){
+                    if(k == j) continue;
+                    mx = min(mx,dp[i-1][k]);
+                }
+                dp[i][j] = g[i][j] + mx;
+            }
+        }
+        
+        int ans = INT_MAX;
+        
+        for(int i=0;i<m;i++){
+            ans = min(ans,dp[n-1][i]);
+        }
+        
+        return ans;
+
+    }
+};
+
