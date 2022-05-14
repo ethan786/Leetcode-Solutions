6b3e17e7efda3aa67330b09f1a69fd54fa61af22
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-14 23:51:39 +0530

    Time: 4 ms (91.51%), Space: 11 MB (58.64%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
+    
     vector<vector<int>> ans;
-    void solve(int i,vector<int>&c,int t,vector<int> &v){
-        
-        if(t<0) return;
-        
-        if(i == c.size()){
+    
+    void solve(int i, vector<int> &a,vector<int> &v, int t){
+        if(t < 0) return;
+        if(i >= a.size()){
             if(t == 0){
                 ans.push_back(v);
             }
             return;
         }
-        t -= c[i];
-        if(t >= 0){
-        v.push_back(c[i]);
-        solve(i,c,t,v);
-        v.pop_back();
-        }
-        t += c[i];
-        solve(i+1,c,t,v);
+        // if(a[i] <= t) {
+            v.push_back(a[i]);
+            solve(i,a,v,t-a[i]);
+            v.pop_back();
+        // }
+        solve(i+1,a,v,t);
     }
     
-    vector<vector<int>> combinationSum(vector<int>& c, int t) {
+    vector<vector<int>> combinationSum(vector<int>& a, int t) {
         vector<int> v;
-        solve(0,c,t,v);
+        solve(0,a,v,t);
         return ans;
     }
 };
