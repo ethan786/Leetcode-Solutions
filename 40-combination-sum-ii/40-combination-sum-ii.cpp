e42f5f678382a2842fcff76734be250d481fa659
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-16 15:21:58 +0530

    Time: 7 ms (71.01%), Space: 10.6 MB (69.00%) - LeetHub

--- 40-combination-sum-ii/40-combination-sum-ii.cpp
+++ 40-combination-sum-ii/40-combination-sum-ii.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     
-    void solve(int i, vector<int> &a, vector<int> &com, vector<vector<int>> &ans, int t){
+    void solve(int i, vector<int> &a, vector<int> &com, vector<vector<int>> &ans, int t) {
         if(t == 0){
             ans.push_back(com);
             return;
         }
         
-        for(int j = i;j<a.size();j++){
-            if(j > i and a[j-1] == a[j]) continue;
-            if(a[j] > t) break;
+        for(int j = i; j < a.size(); j++){
+            if(j > i and a[j] == a[j-1]) continue;
+            if(a[j] > t) return;
             com.push_back(a[j]);
             solve(j+1,a,com,ans,t-a[j]);
             com.pop_back();
         }
-        //solve(i+1,a,com,ans,t);
     }
     
     vector<vector<int>> combinationSum2(vector<int>& a, int t) {
         sort(a.begin(),a.end());
-        vector<int> com;
         vector<vector<int>> ans;
-        solve(0, a, com, ans, t);
+        vector<int> com;
+        solve(0,a,com,ans,t);
         return ans;
     }
 };
