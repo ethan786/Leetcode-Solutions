cbcac559962e2961d42aaf7e3edb5a144cc4a7fa
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-20 00:46:41 +0530

    Time: 29 ms (43.50%), Space: 23.3 MB (48.59%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,40 +1,31 @@
 class Solution {
 public:
-    int c = 0;
-    int dx[4] = {1,-1,0,0};
-    int dy[4] = {0,0,1,-1};
-    void dfs(int i,int j,vector<vector<int>>& g,int n,int m){
-        if(i < 0 || j < 0 || i > n-1 || j > m-1){
-            return;
+    
+    int dx[4] = {0,0,1,-1};
+    int dy[4] = {1,-1,0,0};
+    
+    void dfs(int i, int j, vector<vector<int>> &a, int n, int m,int &area) {
+        if(i < 0 || j < 0 || i > n-1 || j > m-1) return;
+        if(a[i][j] != 1) return;
+        a[i][j] = 2;
+        area++;
+        for(int x = 0; x < 4; ++x) {
+            dfs(i+dx[x],j+dy[x],a,n,m,area);
         }
-        if(g[i][j] != 1){
-            return;
-        }
-        g[i][j] = 2;
-        c++;
-        for(int x=0;x<4;x++){
-            dfs(i+dx[x],j+dy[x],g,n,m);
-        }
-        // dfs(i+1,j,g,n,m);
-        // dfs(i,j-1,g,n,m);
-        // dfs(i-1,j,g,n,m);
-        // dfs(i,j+1,g,n,m);
     }
     
-    int maxAreaOfIsland(vector<vector<int>>& g) {
-        int n = g.size();
-        int m = g[0].size();
-        int mx = 0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(g[i][j] == 1){
-                    //cout<<"X";
-                    dfs(i,j,g,n,m);
-                    mx = max(c,mx);
-                    c=0;
+    int maxAreaOfIsland(vector<vector<int>>& a) {
+        int ans = 0;
+        int n = a.size(), m = a[0].size();
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < m; ++j) {
+                if(a[i][j] == 1) {
+                    int area = 0;
+                    dfs(i,j,a,n,m,area);
+                    ans = max(ans, area);
                 }
             }
         }
-        return mx;
+        return ans;
     }
 };
