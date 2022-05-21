dc95fcbc79420de2a9d32e950fffa0a33e9366dc
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-22 01:00:04 +0530

    Time: 77 ms (58.99%), Space: 21.8 MB (58.31%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,51 +1,38 @@
 class Solution {
 public:
-    int dx[4] = {1,-1,0,0};
-    int dy[4] = {0,0,-1,1};
-    void dfsMarkZeroAllBoundryConnectedComponents(int i,int j,vector<vector<int>>& g,int n,int m){
-        if(i < 0 || j < 0 || i > n-1 || j > m-1){
-            return;
-        }
-        if(g[i][j] != 1){
-            return;
-        }
-        g[i][j] = 0;
-        for(int k=0;k<4;k++){
-            dfsMarkZeroAllBoundryConnectedComponents(i+dx[k],j+dy[k],g,n,m);
+    
+    void dfs(int i, int j, int n, int m, vector<vector<int>>& a) {
+        a[i][j] = 0;
+        int dx[] = {0,0,1,-1};
+        int dy[] = {1,-1,0,0};
+        
+        for(int x = 0; x < 4; ++x) {
+            int ci = i + dx[x];
+            int cj = j + dy[x];
+            if(ci < n and ci >= 0 and cj < m and cj >= 0 and a[ci][cj] == 1){
+                dfs(ci,cj,n,m,a);
+            }
         }
     }
     
-    int numEnclaves(vector<vector<int>>& g) {
-        int n = g.size();
-        int m = g[0].size();
-        int c = 0;
-        for(int i=0;i<n;i++){
-            if(g[i][0] == 1){
-                dfsMarkZeroAllBoundryConnectedComponents(i,0,g,n,m);
-            }
-        }
-        for(int i=0;i<m;i++){
-            if(g[0][i] == 1){
-                dfsMarkZeroAllBoundryConnectedComponents(0,i,g,n,m);
-            }
-        }
-        for(int i=0;i<n;i++){
-            if(g[i][m-1] == 1){
-                dfsMarkZeroAllBoundryConnectedComponents(i,m-1,g,n,m);
-            }
-        }
-        for(int i=0;i<m;i++){
-            if(g[n-1][i] == 1){
-                dfsMarkZeroAllBoundryConnectedComponents(n-1,i,g,n,m);
-            }
-        }
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(g[i][j] == 1){
-                    c++;
+    int numEnclaves(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                if(i == 0 || j == 0 || i == n-1 || j == m-1) {
+                    if(a[i][j] == 1)
+                        dfs(i,j,n,m,a);
                 }
             }
         }
-        return c;
+        int ans = 0;
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < m; ++j) {
+                if(a[i][j] == 1) ans++;
+            }
+        }
+        return ans;
     }
 };
