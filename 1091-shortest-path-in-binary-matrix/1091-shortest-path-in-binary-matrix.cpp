f687224be25fd19954d01def583607b45cbb9551
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-27 00:57:38 +0530

    Time: 73 ms (81.42%), Space: 21.8 MB (36.78%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -1,24 +1,45 @@
 class Solution {
 public:
-    int shortestPathBinaryMatrix(vector<vector<int>>& g, int steps = 0) {
-  queue<pair<int, int>> q;
-  q.push({ 0, 0 });
-  while (!q.empty()) {
-    ++steps;
-    queue<pair<int, int>> q1;
-    while (!q.empty()) {
-      auto c = q.front();
-      q.pop();
-      if (c.first >= 0 && c.second >= 0 && c.first < g.size() && c.second < g.size() && !g[c.first][c.second]) {
-        g[c.first][c.second] = 1;
-        if (c.first == g.size() - 1 && c.second == g.size() - 1) return steps;
-        for (auto i = -1; i < 2; ++i)
-          for (auto j = -1; j < 2; ++j)
-            if (i != 0 || j != 0) q1.push({ c.first + i, c.second + j });
-      }
+    int shortestPathBinaryMatrix(vector<vector<int>>& a) {
+        int n = a.size();
+        int m = a[0].size();
+        
+        if(a[0][0] == 1) return -1;
+        if(a[0][0] == 0 and n == 1 and m == 1) return 1;
+        
+        vector<vector<int>> vis(n,vector<int>(m,0));
+        
+        queue<pair<pair<int,int>,int>> q;
+        q.push({{0,0},1});
+        
+        auto isPossible = [&](int i, int j){
+            if(i < 0 || j < 0 || i >= n || j >= m) return false;
+            return true;
+        };
+        
+        int dx[] = {0,0,1,-1,1,1,-1,-1};
+        int dy[] = {1,-1,1,-1,0,-1,0,1};
+        
+        while(!q.empty()) {
+            auto p = q.front();
+            q.pop();
+            int i = p.first.first;
+            int j = p.first.second;
+            int len = p.second;
+            
+            for(int x = 0; x < 8; x++) {
+                int ci = i + dx[x];
+                int cj = j + dy[x];
+                if(isPossible(ci,cj) and a[ci][cj] == 0 and vis[ci][cj] == 0) {
+                    q.push({{ci,cj},len+1});
+                    vis[ci][cj] = 1;
+                    if(ci == n-1 and cj == m-1) {
+                        return len + 1;
+                    }
+                }
+            }
+            
+        }
+        return -1;
     }
-    swap(q, q1);
-  }
-  return -1;
-}
 };
