d3b93672b447fd10d413ba249d08eb64eed418f8
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-27 21:44:39 +0530

    Time: 7 ms (52.41%), Space: 10.5 MB (21.59%) - LeetHub

---
+++ 958-check-completeness-of-a-binary-tree/958-check-completeness-of-a-binary-tree.cpp
@@ -0,0 +1,70 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int depth(TreeNode* root) {
+        if(!root) return 0;
+        return 1 + max(depth(root->left),depth(root->right));
+    } 
+    
+    bool isCompleteTree(TreeNode* root) {
+        int d = depth(root);
+        if(d == 1) return true;
+        queue<TreeNode*> q;
+        q.push(root);
+        int level = 0;
+        while(level+2 != d) {
+            int n = q.size();
+            
+            if(level != 0) 
+                if(n != pow(2,level)) return false;
+            
+            // cout<<n<<" "<<pow(2,level)<<endl;
+            
+            for(int i=0;i<n;i++) {
+                auto node = q.front();
+                q.pop();
+                if(node->left)
+                    q.push(node->left);
+                if(node->right)
+                    q.push(node->right);
+            }
+            level++;
+        }
+        // cout<<d<<" "<<level<<endl;
+        if(q.size() != pow(2,level)) return false;
+        bool flag = (q.front()->left != NULL);
+        // cout<<q.size()<<endl;
+//         while(!q.empty()) {
+//             cout<<q.front()->val<<endl;
+//             q.pop();
+//         }
+        
+//         return false;
+        
+        while(!q.empty()) {
+            auto node = q.front();
+            q.pop();
+            
+            if((node->left and node->right and flag) || (node->left and flag) || (!node->left and !node->right)) {
+                if(!node->right) {
+                    flag = false;
+                }
+            }else {
+                return false;
+            }
+            
+        }
+        return true;
+    }
+};
