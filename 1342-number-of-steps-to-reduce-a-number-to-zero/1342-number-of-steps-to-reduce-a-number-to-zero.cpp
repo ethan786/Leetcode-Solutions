83d34446eee23c32a4b9a2d3ff31c52e32e47821
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-27 11:48:39 +0530

    Time: 5 ms (14.10%), Space: 6 MB (26.66%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.cpp
@@ -1,6 +1,16 @@
 class Solution {
 public:
-    int numberOfSteps(int num) {
-        return num ? __builtin_popcount(num) + 31 - __builtin_clz(num) : 0;
+    int numberOfSteps (int num) {
+        int count=0;
+        while(num>0){
+        if(num%2==0){
+            num=num/2;
+            count++;
+        }else{
+            num=num-1;
+            count++;
+        }
+        }
+        return count;
     }
 };
