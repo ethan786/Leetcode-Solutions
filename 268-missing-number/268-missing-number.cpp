34afd07f016560213b2774e7dc82248430ffa180
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-05-28 13:38:30 +0530

    Time: 13 ms (96.24%), Space: 18 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+    /* maths solution
     int missingNumber(vector<int>& a) {
         int sum = 0;
         for(int i = 0; i < a.size(); ++i) {
@@ -9,5 +10,13 @@ public:
         long long os = n * (n + 1);
         os /= 2;
         return os - sum;
+    }
+    */
+    int missingNumber(vector<int>& a) {
+        int ans = 0;
+        for(int i = 0; i < a.size(); ++i) {
+            ans ^= i ^ a[i];
+        }
+        return ans^a.size();
     }
 };
