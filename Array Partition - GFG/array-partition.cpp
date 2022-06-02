f82f7317dc1a9b5bdde28fc0923aa5ce91398db0
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-02 23:26:43 +0530

    Added solution - LeetHub

--- Array Partition - GFG/array-partition.cpp
+++ Array Partition - GFG/array-partition.cpp
@@ -9,26 +9,36 @@ using namespace std;
 
 class Solution{
     public:
-    bool solve(int ind, int k, int m, vector<int>& A, vector<int>& dp){
+    bool solve(int ind, int k, int m, vector<int>& a, vector<int>& dp){
         bool flag = false;
-        if(ind <= -1) return true;
-        if(dp[ind] != -1) return dp[ind];
-        for(int i = ind - k + 1; i >= 0 && A[ind] - A[i] <= m; i--){
-            flag = flag | solve(i - 1, k, m, A, dp);
+        if(ind < 0){
+            return true;
+        }
+        
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        
+        for(int i = ind - k + 1;( i >= 0 && a[ind] - a[i] <= m); i--){
+            flag |= solve(i - 1, k, m, a, dp);
             if(flag == true){
                 break;
             }
-        }
+        } 
         dp[ind] = flag;
         return flag;
     }
-    bool partitionArray(int n, int K, int M, vector<int> &A){
-        sort(A.begin(), A.end());
+    bool partitionArray(int n, int k, int m, vector<int> &a){
+        sort(a.begin(), a.end());
         vector<int> dp(n, -1);
-        solve(n - 1, K, M, A, dp);
+        solve(n - 1, k, m, a, dp);
         return dp[n - 1];
     }
      // 1 2 3 8 9
+     //         9 ,8 ,3 (8-3) > m
+     // 9 , 8 
+     // 3, 2
+     // 3, 2, 1
 };
 
 // { Driver Code Starts.
