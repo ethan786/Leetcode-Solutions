8f8101a0ab4d4490a38d92687cfbad78669971d4
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-04 13:19:40 +0530

    Time: 3 ms (96.12%), Space: 7.3 MB (67.79%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,85 +1,24 @@
-// class Solution {
-// public:
-    
-//     bool isSafe(int r,int c,vector<string> &board,int n){
-//         int dr = r;
-//         int dc = c;
-//         //backward direction
-//         while(c>=0){
-//             if(board[r][c]=='Q') return false;
-//             c--;
-//         }
-//         //upper diagonal
-//         r=dr;
-//         c=dc;
-//         while(r>=0 and c>= 0){
-//             if(board[r][c]=='Q') return false;
-//             c--;
-//             r--;
-//         }
-//         //downward diagonal
-//         r=dr;
-//         c=dc;
-//         while(r<n and c>= 0){
-//             if(board[r][c]=='Q') return false;
-//             c--;
-//             r++;
-//         }
-//         return true;
-//     }
-    
-//     void solve(vector<vector<string>> &ans,vector<string> &board,int n,int col){
-//         //base case
-//         if(col == n){
-//             ans.push_back(board);
-//             return;
-//         }
-        
-//         for(int row=0;row<n;row++){
-//             if(isSafe(row,col,board,n)){
-//                 board[row][col] = 'Q';
-//                 solve(ans,board,n,col+1);
-//                 board[row][col] = '.';
-//             }
-//         }
-//     }
-    
-//     vector<vector<string>> solveNQueens(int n) {
-//         vector<vector<string>> ans;
-//         vector<string> board(n);
-//         string s(n,'.');
-//         for(int i=0;i<n;i++){
-//             board[i] = s;
-//         }
-//         solve(ans,board,n,0);
-//         return ans;
-//     }
-// };
-
-
-
 class Solution {
 public:
-    //time complexity optimized
     
-    void solve(int col,int n,vector<vector<string>> &ans,vector<string> &board,vector<bool> &back,vector<bool> &upperdiagonal,vector<bool> &lowerdiagonal){
-        //base case
-        if(col == n){
-            ans.push_back(board);
+    
+    void solve(int n,vector<vector<string>> &ans, vector<string> &a, int c, vector<int>&l, vector<int>&ld, vector<int> &ud) {
+        if(c == n) {
+            ans.push_back(a);
             return;
         }
-        //traversing through every row 
-        for(int row=0;row<n;row++){
-            if(!back[row] and !upperdiagonal[n-1 + col - row] and !lowerdiagonal[col+row]){
-                board[row][col] = 'Q';
-                back[row] = 1;
-                upperdiagonal[n-1 + col - row] = 1;
-                lowerdiagonal[row+col] = 1;
-                solve(col+1,n,ans,board,back,upperdiagonal,lowerdiagonal);
-                board[row][col] = '.';
-                back[row] = 0;
-                upperdiagonal[n-1 + col - row] = 0;
-                lowerdiagonal[row+col] = 0;
+        
+        for(int r = 0; r < n; r++) {
+            if(l[r] == 0 and ld[(c-r) + n-1] == 0 and ud[c+r] == 0) {
+                a[r][c] = 'Q';
+                l[r] = 1;
+                ld[(c-r) + n-1] = 1;
+                ud[c+r] = 1;
+                solve(n,ans,a,c+1,l,ld,ud);
+                a[r][c] = '.';
+                l[r] = 0;
+                ld[(c-r) + n-1] = 0;
+                ud[c+r] = 0;
             }
         }
         
@@ -87,20 +26,15 @@ public:
     
     vector<vector<string>> solveNQueens(int n) {
         vector<vector<string>> ans;
-        vector<string> board(n);
         string s(n,'.');
+        vector<string> a(n);
         for(int i=0;i<n;i++){
-            board[i] = s;
+            a[i] = s;
         }
-        //using containers instead of is safe function
-        //space optimized using bool vector instead of int
-        // vector<int> back(n,0);
-        // vector<int> upperdiagonal(2*n-1,0);
-        // vector<int> lowerdiagonal(2*n-1,0);
-        vector<bool> back(n,0);
-        vector<bool> upperdiagonal(2*n-1,0);
-        vector<bool> lowerdiagonal(2*n-1,0);
-        solve(0,n,ans,board,back,upperdiagonal,lowerdiagonal);
+        vector<int> l(n,0);
+        vector<int> ld(2*n-1,0);
+        vector<int> ud(2*n-1,0);
+        solve(n,ans,a,0,l,ld,ud);
         return ans;
     }
 };
