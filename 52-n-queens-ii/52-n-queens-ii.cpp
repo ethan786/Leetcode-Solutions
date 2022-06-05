636ec73630481d72f1bf5397afd8c68c849e129f
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-05 15:30:45 +0530

    Time: 2 ms (88.87%), Space: 5.9 MB (90.31%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.cpp
+++ 52-n-queens-ii/52-n-queens-ii.cpp
@@ -1,44 +1,22 @@
-class Solution {
+class Solution {  
+private:
+	int res;
 public:
-    int c=0;
-    void solve(int col,int n,vector<string> &board,vector<bool> &back,vector<bool> &upperdiagonal,vector<bool> &lowerdiagonal){
-        //base case
-        if(col == n){
-            c++;
-            return;
-        }
-        //traversing through every row 
-        for(int row=0;row<n;row++){
-            if(!back[row] and !upperdiagonal[n-1 + col - row] and !lowerdiagonal[col+row]){
-                board[row][col] = 'Q';
-                back[row] = 1;
-                upperdiagonal[n-1 + col - row] = 1;
-                lowerdiagonal[row+col] = 1;
-                solve(col+1,n,board,back,upperdiagonal,lowerdiagonal);
-                board[row][col] = '.';
-                back[row] = 0;
-                upperdiagonal[n-1 + col - row] = 0;
-                lowerdiagonal[row+col] = 0;
-            }
-        }
-        
-    }
-    int totalNQueens(int n) {
-       vector<string> board(n);
-        string s(n,'.');
-        for(int i=0;i<n;i++){
-            board[i] = s;
-        }
-        //using containers instead of is safe function
-        //space optimized using bool vector instead of int
-        // vector<int> back(n,0);
-        // vector<int> upperdiagonal(2*n-1,0);
-        // vector<int> lowerdiagonal(2*n-1,0);
-        vector<bool> back(n,0);
-        vector<bool> upperdiagonal(2*n-1,0);
-        vector<bool> lowerdiagonal(2*n-1,0);
-        solve(0,n,board,back,upperdiagonal,lowerdiagonal);
-        return c; 
-    
-}
+	void dfs(int n,int dep,int r,int d1, int d2){
+		if(dep==n){
+			res++;
+			return;
+		}
+		for(int i=0;i<n;i++){
+			int cur=1<<i;
+			if((cur&r) || (cur&d1) || (cur&d2)) continue;
+			dfs(n,dep+1, (cur|r),(cur|d1)<<1,(cur|d2)>>1);
+		}
+
+	}
+	int totalNQueens(int n) {
+		res=0;
+		dfs(n,0,0,0,0);
+		return res;
+	}
 };
