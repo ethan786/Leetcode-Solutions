f47678862027ed35c2114ec98c8ea1bf113d7038
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-10 00:39:25 +0530

    Time: 60 ms (60.56%), Space: 12.2 MB (93.39%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,25 +1,30 @@
 class Solution {
 public:
-    int maximalRectangle(vector<vector<char>>& mat) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> a(n,vector<int>(m,0));
+    int maximalRectangle(vector<vector<char>>& a) {
         
-        if(n==1 and m == 1) {
-            return mat[0][0]-'0';
-        }
+        int n = a.size(), m = a[0].size(), ans = 0;
+        if(n == 0) return 0;
+        vector<int> h(m),l(m,0),r(m,m);
         
-        int ans = 0;
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; ++j) {
-                if(mat[i][j] == '1') {
-                    a[i][j] = j == 0 ? 1 : a[i][j-1] + 1;
-                
-                    int wd = a[i][j];
-                    for(int k = i; k >= 0; k--) {
-                        wd = min(wd,a[k][j]);
-                        ans = max(ans, wd * (i-k+1));
-                    }
-                }
+        for(int i= 0 ; i < n; i++) {
+            int cur_l = 0, cur_r = m;
+            //height
+            for(int j = 0; j < m; j++) {
+                h[j] = (a[i][j] == '1') ? h[j] + 1 : 0; 
+            }
+            //left
+            for(int j = 0; j < m; j++) {
+                l[j] = (a[i][j] == '1') ? max(l[j], cur_l) : 0; 
+                if(a[i][j] != '1') cur_l = j+1;
+            }
+            //right
+            for(int j = m-1; j > -1; j--) {
+                r[j] = (a[i][j] == '1') ? min(r[j], cur_r) : m; 
+                if(a[i][j] != '1') cur_r = j;
+            }
+            //ans
+            for(int j = 0; j < m; j++) {
+                ans = max(ans, h[j] * (r[j] - l[j])); 
             }
         }
         return ans;
