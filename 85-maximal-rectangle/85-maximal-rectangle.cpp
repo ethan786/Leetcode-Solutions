6864a3e15e7a368a5b8012696beb95fecc38c48a
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-09 19:18:36 +0530

    Time: 197 ms (6.33%), Space: 13 MB (82.64%) - LeetHub

--- 85-maximal-rectangle/85-maximal-rectangle.cpp
+++ 85-maximal-rectangle/85-maximal-rectangle.cpp
@@ -1,62 +1,26 @@
 class Solution {
 public:
-    
-    int MAH(vector<int> &a){
-        int n = a.size();
-        vector<int> l(n),r(n);
-        stack<int> s;
-        for(int i=n-1;i>=0;i--){
-                while(!s.empty() and a[s.top()] >= a[i]){
-                    s.pop();
-                }
-                if(s.size() == 0){
-                    l[i] = n - i;
-                }else{
-                    l[i] = s.top() - i;
-                }
-            s.push(i);
+    int maximalRectangle(vector<vector<char>>& mat) {
+        int n = mat.size(), m = mat[0].size();
+        vector<vector<int>> a(n,vector<int>(m,0));
+        
+        if(n==1 and m == 1) {
+            return mat[0][0]-'0';
         }
         
-        while(!s.empty()) s.pop();
-        
-        r[0] = 1;
-        s.push(0);
-        for(int i=1;i<n;i++){
-                while(!s.empty() and a[s.top()] >= a[i]){
-                    s.pop();
-                }
-                if(s.size() == 0){
-                    r[i] = i + 1;
-                }else{
-                    r[i] = i-s.top();
-                }
-            s.push(i);
-        }
-        int ans = -1;
-        for(int i=0;i<n;i++){
-            ans = max(ans,a[i] * (l[i]+r[i]-1));
-        }
-        return ans;
-    }
-    
-    int maximalRectangle(vector<vector<char>>& v) {
-        int n = v.size();
-        int m = v[0].size();
-        vector<int> o(m);
-        for(int i=0;i<m;i++){
-            o[i] = v[0][i] - '0';
-        }
-        int ans = MAH(o);
-        
-        for(int i=1;i<n;i++){
-            for(int j = 0;j<m;j++){
-                if(v[i][j] == '0'){
-                    o[j] = 0;
-                }else{
-                    o[j]++;
+        int ans = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; ++j) {
+                if(mat[i][j] == '1') {
+                    a[i][j] = j == 0 ? 1 : a[i][j-1] + 1;
+                
+                    int wd = a[i][j];
+                    for(int k = i; k >= 0; k--) {
+                        wd = min(wd,a[k][j]);
+                        ans = max(ans, wd * (i-k+1));
+                    }
                 }
             }
-            ans = max(ans,MAH(o));
         }
         return ans;
     }
