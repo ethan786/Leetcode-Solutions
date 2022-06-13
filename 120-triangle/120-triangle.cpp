f460a2ee94ba273f09f539e47fab3fcb9988fa1d
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-13 13:05:41 +0530

    Time: 12 ms (28.50%), Space: 9 MB (26.05%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,30 +1,23 @@
 class Solution {
 public:
-    //recursion + memo
-    // int solve(int i,int j,vector<vector<int>> &t,vector<vector<int>> &dp){
-    //     if(i == t.size()-1){
-    //         return t[i][j];
-    //     }else if(j > t[i].size()-1){
-    //         return INT_MAX;
-    //     }
-    //     if(dp[i][j]!=-1) return dp[i][j];
-    //     return dp[i][j] = t[i][j] + min(solve(i+1,j,t,dp),solve(i+1,j+1,t,dp));
-    // }
-    
-    int minimumTotal(vector<vector<int>>& t) {
-        int n = t.size();
+    int minimumTotal(vector<vector<int>>& a) {
+        int n = a.size();
         vector<vector<int>> dp(n,vector<int>(n,0));
-        //base case
-        for(int i=0;i<n;i++){
-            dp[n-1][i] = t[n-1][i];
+        
+        if(n == 1) return a[0][0];
+        
+        for(int i = 0; i < a[n-1].size(); i++) {
+            dp[n-1][i] = a[n-1][i];
         }
         
-        for(int i=n-2;i>=0;i--){
-            for(int j=i;j>=0;j--){
-                dp[i][j] = t[i][j] + min(dp[i+1][j],dp[i+1][j+1]);
+        for(int i = n-2; i >= 0; i--) {
+            for(int j = 0; j < a[i].size(); j++) {
+                dp[i][j] = a[i][j] + min(dp[i+1][j], dp[i+1][j+1]);
             }
         }
         
         return dp[0][0];
+        
     }
 };
+
