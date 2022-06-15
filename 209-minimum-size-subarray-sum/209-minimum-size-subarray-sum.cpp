23f4e73d27b08467b7258218a98f01057d55a6de
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-15 15:53:23 +0530

    Time: 7 ms (86.01%), Space: 10.6 MB (60.60%) - LeetHub

--- 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
+++ 209-minimum-size-subarray-sum/209-minimum-size-subarray-sum.cpp
@@ -1,23 +1,21 @@
 class Solution {
 public:
-    int minSubArrayLen(int t, vector<int>& arr) {
-        if(arr.size()==1){// for array with single element.
-            return (arr[0]==t)?1:0;
-        }
-        
-        int l=0,r=0,s=0,len=arr.size();
-        while(r<arr.size()){
-            s+=arr[r];// add new element to sum  
-            while(l<=r && s>=t){ // took l<=r to check if single element is >= target ,this loop should run
-                len=min(len,(r-l+1));
-                s-=arr[l];// remove left pointer from sum
-                l++; // increase left pointer as new array starts from next index 
+    int minSubArrayLen(int t, vector<int>& a) {
+        int sum = 0, n = a.size(), ans = INT_MAX;
+        int j = 0;
+        for(int i = 0 ; i < n; i++) {
+            sum += a[i];
+            if(sum >= t) {
+                ans = min(ans, i-j+1);
+                while(sum >= t) {
+                    sum -= a[j];
+                    j++;
+                    if(sum >= t) {
+                        ans = min(ans, i-j+1);
+                    }
+                }
             }
-            r++;
         }
-        if(!l && s<t){ // for array whose total sum is less than target , left pointer is at 0th index and sum is less than target
-            return 0;
-        }
-        return len;
+        return ans == INT_MAX ? 0 : ans;
     }
 };
