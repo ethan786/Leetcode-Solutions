61f405401d02c9f524084bcb41a873a9cd6bf7df
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-16 15:51:58 +0530

    Time: 288 ms (41.39%), Space: 231.5 MB (21.00%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -40,40 +40,67 @@ public:
 //     }
     
     
-        string longestPalindrome(string s) {
-            int n = s.size();
-            int dp[n][n];
-            int len = 0,start = 0;
-            for( int gap = 0 ; gap < n ; gap++ ){
-                for(int i = 0 , j = gap ; j < n ; i++ , j++){
-                    if(gap == 0){
-                        dp[i][j] = true;
-                    }else if(gap == 1 and s[i] == s[j]){
-                        dp[i][j] = true;
-                    }else if(s[i] == s[j] and dp[i+1][j-1] == true){
-                        dp[i][j] = true;
-                    }else{
-                        dp[i][j] = false;
-                    }
-                    //answer
-                    if(dp[i][j]){
-                        start = i;
-                        len = gap+1;
-                    }
-                }
-            }
-            return s.substr(start,len);
-        }
+        // string longestPalindrome(string s) {
+        //     int n = s.size();
+        //     int dp[n][n];
+        //     int len = 0,start = 0;
+        //     for( int gap = 0 ; gap < n ; gap++ ){
+        //         for(int i = 0 , j = gap ; j < n ; i++ , j++){
+        //             if(gap == 0){
+        //                 dp[i][j] = true;
+        //             }else if(gap == 1 and s[i] == s[j]){
+        //                 dp[i][j] = true;
+        //             }else if(s[i] == s[j] and dp[i+1][j-1] == true){
+        //                 dp[i][j] = true;
+        //             }else{
+        //                 dp[i][j] = false;
+        //             }
+        //             //answer
+        //             if(dp[i][j]){
+        //                 start = i;
+        //                 len = gap+1;
+        //             }
+        //         }
+        //     }
+        //     return s.substr(start,len);
+        // }
     
     //Now comes the better approach 
     //instead of checking ends of substring and the 
     //depending on smallersubstring using dp
     //we can start from the centre and then spread out
     
+    int expand(string s, int i, int j) {
+        int ans = 0;
+        while(i >= 0 and j < s.size()) {
+            if(s[i] == s[j]) {
+                if(i == j){
+                    ans++;
+                }else{
+                    ans += 2;
+                }
+                i--;j++;
+            }else{
+                return ans;
+            }
+        }
+        return ans;
+    }
     
-    // string longestPalindrome(string s){
-    //     //sunday this approach
-    // }
+    string longestPalindrome(string s){
+        int n = s.size();
+        int len,mx = -1, st;
+        for(int i = 0; i < n; i++) {
+            int l1 = expand(s,i,i);
+            int l2 = expand(s,i,i+1);
+            len = max(l1,l2);
+            if(len > mx) {
+                mx = len;
+                st = i - (len-1)/2;
+            }
+        }
+        return s.substr(st,mx);
+    }
     
     
     
