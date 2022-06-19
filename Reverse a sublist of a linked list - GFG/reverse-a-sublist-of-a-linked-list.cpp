bcd12e90ef02bb3f924c7cd315b39bf9bc99f22e
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-19 21:23:10 +0530

    Added solution - LeetHub

---
+++ Reverse a sublist of a linked list - GFG/reverse-a-sublist-of-a-linked-list.cpp
@@ -0,0 +1,119 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include<bits/stdc++.h>
+using namespace std;
+
+/* Link list node */
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+/*Link list node 
+struct Node {
+	int data;
+	struct Node *next;
+	Node(int x) {
+		data = x;
+		next = NULL;
+	}
+};*/
+
+class Solution
+{
+    public:
+    Node* reverseBetween(Node* head, int m, int n){
+        int i=1;
+       Node* p=head;
+       //stack save value m->n
+       stack<int> st;
+       while(p!=NULL){
+           if(i>=m && i<=n){
+              st.push(p->data);
+           }
+           i++;
+           p=p->next;
+       }
+       p=head;
+       i=1;
+       //assign reverse between m and n
+       while(p!=NULL){
+           if(i>=m && i<=n){
+                p->data=st.top();
+                st.pop();
+           }
+           if(i==n){
+               break;
+           }
+           i++;
+           p=p->next;
+       }
+       return head;
+   }
+};
+
+// { Driver Code Starts.
+
+/* Function to print linked list */
+void printList(struct Node *head)
+{
+	struct Node *temp = head;
+	while (temp != NULL)
+	{
+		printf("%d ", temp->data);
+		temp = temp->next;
+	}
+}
+
+
+
+// Driver program to test above functions
+int main()
+{
+	int T;
+	cin >> T;
+
+	while (T--)
+	{
+		int N, m, n;
+		cin >> N>>m>>n;
+
+		Node *head = NULL;
+		Node *temp = head;
+
+		for (int i = 0; i < N; i++) {
+			int data;
+			cin >> data;
+			if (head == NULL)
+				head = temp = new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+
+		
+
+        Solution ob;
+
+		Node* newhead = ob.reverseBetween(head, m, n);
+		printList(newhead);
+
+		cout << "\n";
+
+
+
+	}
+	return 0;
+}
+  // } Driver Code Ends
