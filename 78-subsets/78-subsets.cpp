2fe70d2f0bb6d818b5eaaaea50b81a79148b3ff6
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-21 18:37:51 +0530

    Time: 12 ms (8.36%), Space: 7.1 MB (69.52%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
-    vector<vector<int>> ans;
-    
-    void sub(vector<int> &nums, int i, vector<int> temp)
-    {
-        if(i==nums.size())
-        {
-            ans.push_back(temp);
-            return;
+    vector<vector<int>> subsets(vector<int>& a) {
+        
+        vector<vector<int>> ans;
+        int n = a.size();
+        for(int bits = 0; bits < (1<<n); bits++) {
+            vector<int> subset;
+            for(int i = 0; i < n; ++i) {
+                if(bits & (1 << i)) {
+                    subset.push_back(a[i]);
+                }
+            }
+            ans.push_back(subset);
         }
         
-        sub(nums, i+1, temp);
-        temp.push_back(nums[i]);
-        sub(nums, i+1, temp);
-    }
-    
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> temp;       
-        sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
         return ans;
     }
 };
