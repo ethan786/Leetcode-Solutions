beaf9e1f7921f1257522c2cd23e6eb58caa74b99
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-22 20:20:56 +0530

    Time: 3 ms (53.08%), Space: 7.8 MB (46.76%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,46 +1,20 @@
 class Solution {
 public:
-//     //recursion
-//     int solve(vector<int>& nums,int i,int n){
-//         if(i>=n){
-//             return 0;
-//         }
-//         int op1 = solve(nums,i+2,n) + nums[i];
-//         int op2 = solve(nums,i+1,n);
-//         return max(op1,op2);
-//     }
-    
-//     int rob(vector<int>& nums) {
-//         return solve(nums,0,nums.size());
-//     }
-    
-    //    // memo
-    //    int solve(vector<int>& nums,int i,int n,vector<int>& dp){
-    //        if(i>=n){
-    //            return 0;
-    //        }
-    //        if(dp[i]!=-1) return dp[i];
-    //        int op1 = solve(nums,i+2,n,dp) + nums[i];
-    //        int op2 = solve(nums,i+1,n,dp);
-    //        return dp[i] = max(op1,op2);
-    //    }
-    //    int rob(vector<int>& nums) {
-    //         int n = nums.size();
-    //         vector<int> dp(n+1,-1);
-    //         return solve(nums,0,n,dp);
-    // }
-    //tabulation
-       int rob(vector<int>& nums) {
-           int n = nums.size();
-           if(n==1) return nums[0];
-           if(n==2) return max(nums[0],nums[1]);
-           vector<int> dp(n,0);
-           dp[0] = nums[0];
-           dp[1] = max(nums[0],nums[1]);
-           for(int i=2;i<n;i++){
-               dp[i] = max(nums[i]+dp[i-2],dp[i-1]);
-           }
-           return dp[n-1];
+    int rob(vector<int>& a) {
+        int n = a.size();
+        if(n == 1) {
+            return a[0];
+        }else if(n == 2) {
+            return max(a[0], a[1]);
+        }
+        vector<int> dp(n+1,0);
+        dp[0] = a[0];
+        dp[1] = max(a[0],a[1]);
+
+        for(int i = 2; i < n; i++) {
+            dp[i] = max(dp[i-1], dp[i-2] + a[i]);
+        }
+
+        return dp[n-1];
     }
-    
 };
