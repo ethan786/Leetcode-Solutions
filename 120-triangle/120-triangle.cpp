bc8eb73bf33e683afaddc88da2d28fe38d2f2495
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-23 17:13:47 +0530

    Time: 10 ms (50.46%), Space: 8.6 MB (88.49%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -3,8 +3,6 @@ public:
     int minimumTotal(vector<vector<int>>& a) {
         int n = a.size();
         
-        if(n == 1) return a[0][0];
-        
         for(int i = n-2; i >= 0; --i) {
             for(int j = 0; j < a[i].size(); ++j) {
                 a[i][j] += min(a[i+1][j], a[i+1][j+1]);
