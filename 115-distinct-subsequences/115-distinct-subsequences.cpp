7baa41e965b6205a851d55c0b7e5fc65170a3d08
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-26 19:10:38 +0530

    Time: 104 ms (10.06%), Space: 30.2 MB (16.69%) - LeetHub

--- 115-distinct-subsequences/115-distinct-subsequences.cpp
+++ 115-distinct-subsequences/115-distinct-subsequences.cpp
@@ -1,25 +1,36 @@
 class Solution {
 public:
     int mod = 1000000007;
-long long solve(int i,int j,string &a,string &b,int n,int m,vector<vector<int>> &dp) {
-    if(i == n and j == m) {
-        return 1;
-    }
-    if(i > n || j > m) return 0;
-    
-    if(dp[i][j] != -1) return dp[i][j];
-    
-    long long x = 0;
-    if(a[i] == b[j]) {
-        x = solve(i+1,j+1,a,b,n,m,dp);
-    }
-    long long y = solve(i+1,j,a,b,n,m,dp);
-    return dp[i][j] = (x + y) % mod;
-}
+    long long solve(int i,int j,string &a,string &b,int n,int m,vector<vector<int>> &dp) {
+        if(i == n and j == m) {
+            return 1;
+        }
+        if(i > n || j > m) return 0;
 
-int numDistinct(string a, string b) {
-    int n = a.size(), m = b.size();
-    vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-    return (int)solve(0,0,a,b,a.size(),b.size(),dp);
-} 
+        if(dp[i][j] != -1) return dp[i][j];
+
+        long long x = 0;
+        if(a[i] == b[j]) {
+            x = solve(i+1,j+1,a,b,n,m,dp);
+        }
+        long long y = solve(i+1,j,a,b,n,m,dp);
+        return dp[i][j] = (x + y) % mod;
+    }
+
+    int numDistinct(string a, string b) {
+        int n = a.size(), m = b.size();
+        vector<vector<double>> dp(n+1,vector<double>(m+1,0));
+        for(int i = 0 ; i <= n; i++) dp[i][0] = 1; //b is empty
+        for(int i = 1 ; i <= m; i++) dp[0][i] = 0;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(a[i-1] == b[j-1]) {
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j];
+                }else {
+                    dp[i][j] = dp[i-1][j];
+                }
+            }
+        }
+        return (int)dp[n][m];
+    } 
 };
