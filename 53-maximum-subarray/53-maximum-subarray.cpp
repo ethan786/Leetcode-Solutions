6a98f2b1305c50a2df4579806de222703c50fb80
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-27 13:11:05 +0530

    Time: 154 ms (54.85%), Space: 67.7 MB (90.41%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,46 +1,14 @@
 class Solution {
 public:
-    //o(n^2) approach finding sum of all subarray 
-    // int maxSubArray(vector<int>& nums) {
-    //     int n = nums.size();
-    //     int ans = INT_MIN;
-    //     for(int s=0;s<n;s++){
-    //         for(int e=s;e<=n;e++){
-    //             int sum=0;
-    //             for(int k=s;k<e;k++){
-    //                 sum+=nums[k];
-    //                 ans = max(ans,sum);
-    //             }
-    //         }
-    //     }
-    //     return ans;
-    // }
-    int maxSubArray(vector<int>& nums) {
-        int ans = INT_MIN;
-        int n = nums.size();
-        int sum = 0;
-        for(int i =0;i<n;i++){
-            sum += nums[i];
-            ans = max(sum,ans);
-            if(sum<0){
-                sum=0;
+    int maxSubArray(vector<int>& a) {
+        int n = a.size(), ans = INT_MIN, cur = 0;
+        for(int i = 0; i < n; i++) {
+            cur += a[i];
+            ans = max(cur, ans);
+            if(cur < 0) {
+                cur = 0;
             }
         }
         return ans;
     }
-    
-    //o(n) approach kadane's algorithm
-    // int maxSubArray(vector<int>& nums) {
-    //     int ans = INT_MIN;
-    //     int n = nums.size();
-    //     int sum = 0;
-    //     for(int i =0;i<n;i++){
-    //         sum += nums[i];
-    //         ans = max(sum,ans);
-    //         if(sum<0){
-    //             sum=0;
-    //         }
-    //     }
-    //     return ans;
-    // }
 };
