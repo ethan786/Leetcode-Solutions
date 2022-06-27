85b2787b73106d9989ed64b76b34355e7d8a7ddc
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-27 13:51:00 +0530

    Time: 255 ms (13.30%), Space: 93.3 MB (52.05%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,36 +1,15 @@
 class Solution {
 public:
-    
-    int solve(int i,bool isBuy,int t,vector<int> &p,int n,vector<vector<int>> &dp){
-        if(i>=n || t <= 0){
-            return 0;
+    int maxProfit(vector<int>& a) {
+        int ans = 0;
+        int buy = a[0];
+        for(int i = 1; i < a.size(); i++) {
+            if(buy > a[i]) {
+                buy = a[i];
+            }else {
+                ans = max(a[i] - buy, ans);
+            }
         }
-        if(dp[i][isBuy] != -1){
-            return dp[i][isBuy];
-        }
-        if(isBuy == false){
-            int op1 = solve(i+1,!isBuy,t,p,n,dp) - p[i];
-            int op2 = solve(i+1,isBuy,t,p,n,dp);
-            //cout<<op1<<" "<<op2<<endl;
-            return dp[i][isBuy] = max(op1,op2);
-        }else{
-            int op1 = solve(i+1,!isBuy,t-1,p,n,dp) + p[i];
-            int op2 = solve(i+1,isBuy,t,p,n,dp);
-            //cout<<op1<<" "<<op2<<"wwe"<<endl;
-            return dp[i][isBuy] = max(op1,op2);
-        }
-    }
-    
-    int maxProfit(vector<int>& p) {
-        int n = p.size();
-        if(n==1){
-            return 0;
-        }else if(n==2){
-            return max(0,p[1]-p[0]);
-        }
-        bool isBuy = false;
-        int trans = 1;
-        vector<vector<int>> dp(n,vector<int>(2,-1));
-        return solve(0,isBuy,trans,p,n,dp);
+        return max(ans,0);
     }
 };
