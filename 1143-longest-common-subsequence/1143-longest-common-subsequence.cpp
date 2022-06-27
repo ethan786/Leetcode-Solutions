140de4ded1500903c7ef71b52c6cfe58a8d5b8be
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-27 18:14:06 +0530

    Time: 29 ms (61.26%), Space: 13 MB (40.28%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -4,8 +4,8 @@ public:
         int n = a.size(), m = b.size();
         vector<vector<int>> dp(n+1,vector<int>(m+1,0));
         
-        for(int i = 0; i <= n; i++) dp[i][0] = 0;
-        for(int i = 0; i <= m; i++) dp[0][i] = 0;
+        // for(int i = 0; i <= n; i++) dp[i][0] = 0;
+        // for(int i = 0; i <= m; i++) dp[0][i] = 0;
         
         for(int i = 1; i <= n; i++) {
             for(int j = 1; j <= m; j++) {
