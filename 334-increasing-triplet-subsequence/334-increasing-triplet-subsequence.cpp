a5ab0b0c11bae3db2b5c7124d162690de57f3d3d
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-06-28 17:26:40 +0530

    Time: 190 ms (5.27%), Space: 61.6 MB (27.50%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool increasingTriplet(vector<int>& a) {
+        int n = a.size();
+        vector<int> temp;
+        temp.push_back(a[0]);
+        int ans = 1;
+        for(int i = 1; i < n; i++) {
+            if(temp.back() < a[i]) {
+                temp.push_back(a[i]);
+                ans++;
+            }else {
+                int ind = lower_bound(temp.begin(), temp.end(), a[i]) - temp.begin();
+                temp[ind] = a[i];
+            }
+        }
+        return ans >= 3;
+    }
+};
