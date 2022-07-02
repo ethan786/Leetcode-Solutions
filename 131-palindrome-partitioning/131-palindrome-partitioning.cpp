65cc1d9edb692469bcb8429256321983ffbdc922
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-07-02 13:58:19 +0530

    Time: 268 ms (30.39%), Space: 79.9 MB (35.23%) - LeetHub

--- 131-palindrome-partitioning/131-palindrome-partitioning.cpp
+++ 131-palindrome-partitioning/131-palindrome-partitioning.cpp
@@ -1,41 +1,34 @@
 class Solution {
 public:
     
-    bool isPalindrome(string s){
-        int i = 0, j = s.size()-1;
-        while(i<j){
+    vector<vector<string>> ans;
+    
+    bool isPalindrome(int i, int j, string s) {
+        while(i < j) {
             if(s[i] != s[j]) return false;
-            i++;
-            j--;
+            i++;j--;
         }
         return true;
     }
-    int n;
-    void solve(int i, string s,vector<string> &com, vector<vector<string>> &ans) {
-        // base condition
-        if(i == n){
-            ans.push_back(com);
-            return;
+    
+    void solve(int i, string &s, vector<string> &p) {
+        if(i == s.size()) {
+            ans.push_back(p);
         }
         
-        for(int j = i; j < s.size(); ++j){
-            string x = s.substr(i,j-i+1);
-            // cout<<x<<" "<<y<<endl;
-            if(isPalindrome(x)){
-                com.push_back(x);
-                solve(j+1,s,com,ans);
-                com.pop_back();
+        for(int j = i; j < s.size(); j++) {
+            if(isPalindrome(i,j,s)) {
+                p.push_back(s.substr(i,j-i+1));
+                solve(j+1,s,p);
+                p.pop_back();
             }
         }
         
     }
     
     vector<vector<string>> partition(string s) {
-        n = s.size();
-        vector<string> com;
-        vector<vector<string>> ans;
-        solve(0,s,com,ans);
+        vector<string> p;
+        solve(0,s,p);
         return ans;
     }
 };
-
