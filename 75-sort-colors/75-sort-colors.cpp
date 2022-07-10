a28e6dd4adca21aae1de93cbff995e5d5585effa
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-07-10 22:09:52 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (90.44%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,16 +1,23 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> freq(3,0); // 0 1 2
-        for(auto &i : nums) {
-            freq[i]++;
-        }
-        int k = 0;
-        for(int i = 0; i < 3; i++) {
-            int count = freq[i];
-            for(int j = 0; j < count; j++) {
-                nums[k++] = i;
+    void sortColors(vector<int>& a) {
+        // 0 0 2 1 1 2
+        // i         k
+        // j
+        // i - > 0
+        // j - > 1
+        // k - > 2
+        int n = a.size();
+        int i = 0, j = 0, k = n-1;
+        
+        while(j <= k) // =
+        {
+            if(a[j] == 0) {
+                swap(a[i++],a[j++]);
+            }else if(a[j] == 1) {
+                j++;
+            }else {
+                swap(a[j],a[k--]);
             }
         }
         
