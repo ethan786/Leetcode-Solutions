6599cb7f682dbf23540d498b78b58387354039f0
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-07-20 14:31:46 +0530

    Time: 667 ms (24.69%), Space: 50 MB (45.03%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -3,27 +3,32 @@ public:
     int numMatchingSubseq(string s, vector<string>& words) {
         int n = s.size();
         map<char,vector<int>> m;
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < n; i++) {
             m[s[i]].push_back(i);
         }
+        
         int ans = 0;
-        bool flag = true;
-        for(auto &i : words){
-            flag = true;
+        
+        for(string &i : words) {
             string cur = i;
             int latest = -1;
-            for(auto &j : cur){
-                auto it = upper_bound(m[j].begin(),m[j].end(),latest);
-                if(it == m[j].end()){
+            bool flag = true;
+            for(char &j : i) {
+                auto it = upper_bound(m[j].begin(), m[j].end(), latest);
+                if(it == m[j].end()) {
                     flag = false;
-                    break;
                 }
+                if(!flag) break;
                 latest = *it;
             }
-            if(flag){
-                ans++;
-            }
+            if(flag) ans++;
         }
         return ans;
     }
 };
+
+// aaaaaaaa.....b
+// aaaab
+
+// we are doing so much of time in finding b
+// instead of finding b we can store the position of b
