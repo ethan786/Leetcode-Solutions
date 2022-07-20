3ee3680b075e841979367d2f6a261ba8f52bccbe
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-07-21 02:00:59 +0530

    Time: 175 ms (5.06%), Space: 66.3 MB (8.28%) - LeetHub

--- 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.cpp
@@ -2,28 +2,21 @@ class Solution {
 public:
     bool increasingTriplet(vector<int>& a) {
         int n = a.size();
-        // vector<int> temp;
-        // temp.push_back(a[0]);
-        // int ans = 1;
-        // for(int i = 1; i < n; i++) {
-        //     if(temp.back() < a[i]) {
-        //         temp.push_back(a[i]);
-        //         ans++;
-        //     }else {
-        //         int ind = lower_bound(temp.begin(), temp.end(), a[i]) - temp.begin();
-        //         temp[ind] = a[i];
-        //     }
-        // }
-        // return ans >= 3;
-        int x = INT_MAX, y = INT_MAX;
+        stack<int> s;
+        vector<int> left(n,-1), right(n,n);
         for(int i = 0; i < n; i++) {
-            if(a[i] <= x) {
-                x = a[i];
-            }else if(a[i] <= y) {
-                y = a[i];
-            }else {
-                return true;
-            }
+            while(!s.empty() and a[s.top()] >= a[i]) s.pop();
+            left[i] = (s.empty() ? -1 : s.top());
+            s.push(i);
+        }
+        while(!s.empty()) s.pop();
+        for(int i = n-1; i >= 0; i--) {
+            while(!s.empty() and a[s.top()] <= a[i]) s.pop();
+            right[i] = (s.empty() ? n : s.top());
+            s.push(i);
+        }
+         for(int i = 1; i < n-1; i++) {
+             if(left[i] >= 0 and right[i] <= n-1) return true;
         }
         return false;
     }
