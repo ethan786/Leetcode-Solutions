0c8f45dcfeae538bb59510dfcf462230dd2a33a9
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-07-30 12:19:02 +0530

    Time: 207 ms (24.82%), Space: 67.8 MB (10.91%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,14 +1,21 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& a) {
-        int n = a.size(), ans = INT_MIN, cur = 0;
-        for(int i = 0; i < n; i++) {
-            cur += a[i];
-            ans = max(cur, ans);
-            if(cur < 0) {
-                cur = 0;
+        int n = a.size();
+        // like we have to maximize the sum 
+        // so, instead of taking negative in the sum 
+        // we will use postive element in the sum 
+        // as possible
+        int curSum = 0, ans = INT_MIN;
+        
+        for(auto &i : a) {
+            curSum += i;
+            ans = max(ans, curSum);
+            if(curSum <= 0) {
+                curSum = 0;
             }
         }
+        
         return ans;
     }
 };
