68b4a6ebb6756ab6797cf7efb987d56d4c28f1c5
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-07 16:21:28 +0530

    Time: 133 ms (48.75%), Space: 69.5 MB (52.67%) - LeetHub

--- 134-gas-station/134-gas-station.cpp
+++ 134-gas-station/134-gas-station.cpp
@@ -1,55 +1,15 @@
-// class Solution {
-// public:
-//     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-//         int n = gas.size();
-//         for(int i=0;i<n;i++){
-//             if(gas[i] >= cost[i]){
-//                 int c = 0;
-//                 int cur_tank_cap = gas[i];
-//                 int j = i;
-//                 // while(c!=n){
-//                 //     cur_tank_cap -= cost[j%n];
-//                 //     //cout<<"1->"<<cur_tank_cap<<" ";
-//                 //     if(cur_tank_cap<0) break;
-//                 //     j++;
-//                 //     c++;
-//                 //     cur_tank_cap += gas[j%n];
-//                 //     //cout<<"2->"<<cur_tank_cap<<" "<<endl;
-//                 // }
-                
-//                 int next_station = 
-                
-//                 if(c==n){
-//                     return i;
-//                 }
-//             }
-//         }
-//         return -1;
-//     }
-// };
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
-        int current = 0;
-        int total = 0;
-        int diff = 0;
-        int start = 0;
-
-        for(int i = 0; i < gas.size(); i++){
-            diff = gas[i] - cost[i];
-
-            total += diff;
-            current += diff;
-            if(current < 0){
-                start = i + 1;
-                current = 0;
+        if(accumulate(gas.begin(), gas.end(),0) < accumulate(cost.begin(), cost.end(),0)) return -1;
+        int rem = 0, ans = 0;
+        for(int i = 0; i < gas.size(); i++) {
+            rem += gas[i] - cost[i];
+            if(rem < 0) {
+                rem = 0;
+                ans = i+1;
             }
         }
-
-        if(total >= 0){
-            return start;
-        }
-
-        return -1;
-    }
+        return ans;
+    } 
 };
