958c27180ec322a4f7ac39a639aab6a0de596c17
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-13 18:21:41 +0530

    Time: 910 ms (47.95%), Space: 299.7 MB (15.98%) - LeetHub

---
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -0,0 +1,24 @@
+int MX = 2e5 + 2;
+class Solution {
+public:
+    int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
+        int i, l, r, ans = 0;
+        startPos++; // incremented positions by one to make calculations easier.
+        vector<int> prefix_sum(MX);
+        for(auto &it: fruits) prefix_sum[it[0] + 1] = it[1];
+        for(i = 1; i < MX; i++) prefix_sum[i] += prefix_sum[i - 1];
+        
+        for(r = startPos; r < MX && r <= startPos + k; r++){
+            l = min(startPos, startPos - (k - 2 * (r - startPos)));
+            l = max(1, l);
+            ans = max(ans, prefix_sum[r] - prefix_sum[l - 1]);
+        }
+        
+        for(l = startPos; l > 0 && l >= startPos - k; l--){
+            r = max(startPos, startPos + (k - 2 * (startPos - l)));
+            r = min(MX - 1, r);
+            ans = max(ans, prefix_sum[r] - prefix_sum[l - 1]);
+        }
+        return ans;
+    }
+};
