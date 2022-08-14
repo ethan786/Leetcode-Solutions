776511c939ab2c1f75aa0ae36d1801d6014e82cc
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-14 16:24:24 +0530

    Time: 153 ms (46.82%), Space: 14.5 MB (67.08%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-    int solve(int i, vector<int> &a, int t, vector<vector<int>> &dp) {
-        if(i == a.size()){
-            if(t == 0) {
-                return 0;
+    int coinChange(vector<int>& a, int k) {
+        vector<int> dp(k+1,0);
+        sort(begin(a), end(a));
+        for (int i = 1; i <= k; i++) {
+            dp[i] = INT_MAX;
+            for (int &c: a) {
+                if(i-c < 0) break;
+                if (dp[i - c] != INT_MAX) 
+                    dp[i] = min(dp[i], 1 + dp[i - c]);
             }
-            return INT_MAX-10;
         }
-        
-        if(dp[i][t] != -1) {
-            return dp[i][t];
-        }
-        
-        int op1 = INT_MAX;
-        if(t >= a[i]) {
-            op1 = 1 + solve(i,a,t-a[i],dp);
-        }
-        int op2 = solve(i+1,a,t,dp);
-        return dp[i][t] = min(op1,op2);
-    }
-    
-    int coinChange(vector<int>& a, int t) {
-        vector<vector<int>> dp(a.size() + 1,vector<int>(t+1,-1));
-        int ans = solve(0,a,t,dp);
-        int mx = (ans == INT_MAX - 10) ? -1 : ans; 
-        return mx;
+        // for(auto &i : dp) cout<<i<<endl;
+        return dp[k] != INT_MAX ? dp[k] : -1;
     }
 };
+
+// 1 2 3 ... 11
