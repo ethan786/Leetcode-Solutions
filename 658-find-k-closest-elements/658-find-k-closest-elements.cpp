88d9c1c80eb7d17497d41e056ab209fa2a7efc2a
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-15 23:48:18 +0530

    Time: 53 ms (86.73%), Space: 31 MB (97.48%) - LeetHub

--- 658-find-k-closest-elements/658-find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/658-find-k-closest-elements.cpp
@@ -1,16 +1,21 @@
 class Solution {
 public:
     vector<int> findClosestElements(vector<int>& a, int k, int x) {
-        int n = a.size(), j = 0; 
-        vector<pair<int,int>> p(n);
-        for(auto &i : a) {
-            p[j++] = {abs(i-x),i};
-        }
-        sort(p.begin(), p.end());
+        int ind = lower_bound(a.begin(), a.end(), x) - a.begin();
+        int i = ind-1, n = a.size();
+        int j = ind;
+        int l = 0;
         vector<int> ans(k);
-        j = 0;
-        for(int i = 0; j < k; i++, j++) {
-            ans[j] = p[i].second;
+        while(l != k) {
+            int left = (i >= 0) ? abs(a[i] - x) : INT_MAX;
+            int right = (j < n) ? abs(a[j] - x) : INT_MAX;
+            if(left > right) {
+                // cout<<l<<" "<<a[j]<<endl;
+                ans[l++] = a[j++];
+            }else {
+                // cout<<l<<" "<<a[i]<<endl;
+                ans[l++] = a[i--];
+            }
         }
         sort(ans.begin(), ans.end());
         return ans;
