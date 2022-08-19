cbc713d706120e6cdf99f47ba465b907f0330b40
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-19 17:51:10 +0530

    Time: 4 ms (57.44%), Space: 7.2 MB (57.04%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,19 +1,15 @@
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& a) {
-        
         vector<vector<int>> ans;
         int n = a.size();
-        for(int bits = 0; bits < (1<<n); bits++) {
-            vector<int> subset;
-            for(int i = 0; i < n; ++i) {
-                if(bits & (1 << i)) {
-                    subset.push_back(a[i]);
-                }
+        for(int bit = 0; bit < 1 << n; bit++) {
+            vector<int> subsets;
+            for(int i = 0; i < 32; i++) {
+                if((1 << i) & bit) subsets.push_back(a[i]);
             }
-            ans.push_back(subset);
+            ans.push_back(subsets);
         }
-        
         return ans;
     }
 };
