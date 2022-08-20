b2e4e47a2a9f54a210bec583efe5b51b7c2ba86a
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-20 17:45:13 +0530

    Time: 3 ms (44.60%), Space: 6.5 MB (46.14%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,31 +1,13 @@
 class Solution {
 public:
-    int path(int n,int m,int i,int j,vector<vector<int>> &dp){
-	if(i==n-1 and j==m-1){
-		return 1;
-	}
-	if(i>n||j>m) return 0;
-	//move forward
-        
-        if(dp[i][j]!=-1) return dp[i][j];
-        
-	int ans = path(n,m,i,j+1,dp) + path(n,m,i+1,j,dp);
-	return dp[i][j] = ans;
-    }
+    const int mod = 2e9;
     int uniquePaths(int m, int n) {
-        vector<vector<int>> dp(101,vector<int>(101,-1));
-        //return path(n,m,0,0,dp);
-        for(int i=0;i<n;i++){
-            dp[i][0] = 1;
-        }
-        for(int i=0;i<m;i++){
-            dp[0][i] = 1;
-        }
-        for(int i=1;i<n;i++){
-            for(int j=1;j<m;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        vector<vector<int>> dp(m, vector<int>(n,1));
+        for(int i = 1; i < m; i++) {
+            for(int j = 1; j < n; j++) {
+                dp[i][j] = (dp[i-1][j] + dp[i][j-1]) % mod;
             }
         }
-        return dp[n-1][m-1];
+        return dp[m-1][n-1] % mod;
     }
 };
