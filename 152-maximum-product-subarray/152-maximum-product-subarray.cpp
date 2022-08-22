5ed7d65471c4d3ff58d3aaddb0d7e542a887d159
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-23 01:27:25 +0530

    Time: 13 ms (45.20%), Space: 13.8 MB (25.90%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -1,16 +1,33 @@
 class Solution {
 public:
     int maxProduct(vector<int>& a) {
-        int n = a.size();
-        int mx = a[0];
-        int mi = a[0];
-        int ans = mx;
-        for(int i = 1; i < n ; i++) {
-            int x = max({a[i], a[i] * mx, a[i] * mi});
-            mi = min({a[i], a[i] * mx, a[i] * mi});
-            mx = x;
-            ans = max(mx,ans); 
+        int mx = a[0], mi = a[0], ans = a[0];
+        for(int i = 1; i < a.size(); i++) {
+            if(a[i] < 0) swap(mx,mi);
+            mx = max(a[i], a[i] * mx);
+            mi = min(a[i], a[i] * mi);
+            ans = max({ans, mx});
         }
         return ans;
     }
+    
+// int maxProduct(vector<int>& nums) {
+        
+//         int n = nums.size();
+//         int ans = nums[0];
+//         int ma = ans;
+//         int mi = ans;
+        
+//         for(int i=1;i<n;i++){
+//             if(nums[i]<0){
+//                 swap(ma,mi);
+//             }
+//             ma = max(nums[i],ma*nums[i]);
+//             mi = min(nums[i],mi*nums[i]);
+//             ans = max(ans,ma);
+//         }
+        
+        
+//         return ans;
+//     }
 };
