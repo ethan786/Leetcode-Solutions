92dafffad3f3584266b2ce050c09780332e1186c
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-08-24 16:49:54 +0530

    Time: 20 ms (25.88%), Space: 17.5 MB (19.95%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -13,27 +13,25 @@ class Solution {
 public:
     int widthOfBinaryTree(TreeNode* root) {
         #define int int64_t
-        if(!root) return 0;
-        
         int ans = 0;
-        queue<pair<TreeNode*, int>> q;
-        q.push({root,0});
-        
+        queue<pair<TreeNode*,int>> q;
+        q.push({root, 0});
+        int first = 0, last = 0;
         while(!q.empty()) {
-            int n = q.size();
-            int mini = q.front().second; // to make start from 0;
-            int first,last;
-            for(int i = 0; i < n; i++) {
-                int cur_id = q.front().second - mini;
-                TreeNode* node = q.front().first;
+            int min = first;
+            int sz = q.size();
+            for(int i = 0; i < sz; i++) {
+                auto x = q.front();
                 q.pop();
-                cout<<cur_id<<" "<<n<<" "<<mini<<endl;
-                if(i == 0) first = cur_id;
-                if(i == n-1) last = cur_id;
-                if(node->left) q.push({node->left, 2*cur_id+1});
-                if(node->right) q.push({node->right, 2*cur_id+2});
+                TreeNode* node = x.first;
+                int ind = x.second;
+                if(i == 0) first = ind;
+                else if(i == sz-1) last = ind;
+                if(node->left) q.push({node->left, 2*ind + 1 - min});
+                if(node->right) q.push({node->right, 2*ind + 2 - min});
             }
-            ans = max(ans,last - first + 1);
+            // cout<<first<<" "<<last<<endl;
+            ans = max(ans, last - first + 1);
         }
         #undef int
         return ans;
