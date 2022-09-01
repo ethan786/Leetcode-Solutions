2509cafe2c3bd1b1ee6fc9476c5794b80f3f35d0
Himanshu Singh <75501866+ethan786@users.noreply.github.com>
2022-09-02 03:22:07 +0530

    Time: 100 ms (26.50%), Space: 18.8 MB (52.01%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -1,47 +1,37 @@
 class Solution {
 public:
+    
     int dx[4] = {1,-1,0,0};
     int dy[4] = {0,0,1,-1};
-    void dfs(int i, int j, vector<vector<int>>& a, int prev, vector<vector<int>>& o) {
-        if(i < 0 || j < 0 || i >= a.size() || j >= a[0].size()) {
-            return;
-        }
-        if(o[i][j] == 1 || a[i][j] < prev) {
-            return;
-        }
-        o[i][j] = 1;
+    
+    void solve(int i, int j, vector<vector<int>> &x, vector<vector<int>> &a, int p) {
+        if(min(i,j) < 0 || i >= a.size() || j >= a[0].size() || a[i][j] < p || x[i][j] == 1) return;
+        x[i][j] = 1;
+        // cout<<i<<" "<<j<<endl;
         for(int k = 0; k < 4; k++) {
-            dfs(i + dx[k], j + dy[k], a, a[i][j], o);
+            int ci = i + dx[k], cj = j + dy[k];
+            solve(ci,cj,x,a,a[i][j]);
         }
     }
     
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& a) {
-        int n = a.size();
-        int m = a[0].size();
-        
-        if(n < 1) return {{}};
-        
-        vector<vector<int>> p(n, vector<int>(m,0)),al(n, vector<int>(m,0));
-        
-        for(int i = 0; i < m; i++) {
-            dfs(0,i,a,INT_MIN,p);
-            dfs(n-1,i,a,INT_MIN,al);
-        }
+        int n = a.size(), m = a[0].size();
+        vector<vector<int>> x(n,vector<int>(m,0)), y(n,vector<int>(m,0));
         for(int i = 0; i < n; i++) {
-            dfs(i,0,a,INT_MIN,p);
-            dfs(i,m-1,a,INT_MIN,al);
+            solve(i,0,x,a,-1);
+            solve(i,m-1,y,a,-1);
         }
-        
+        for(int i = 0; i < m; i++) {
+            solve(0,i,x,a,-1);
+            solve(n-1,i,y,a,-1);
+        }
         vector<vector<int>> ans;
         
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < m; ++j) {
-                if(al[i][j] == 1 and p[i][j] == 1) {
-                    ans.push_back({i,j});
-                }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
+                if(x[i][j] == 1 and y[i][j] == 1) ans.push_back({i,j});
             }
         }
         return ans;
     }
-        
 };
